833ee8648a53a5902ab0b816806af43fb35da5e8
AReid987 <read.musik@gmail.com>
2018-05-23 21:00:12 -0500

    add new and create action to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        session[:user_id] = @user.id
+        format.html { redirect_to user_path(@user), notice: "Sign Up Success" }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
