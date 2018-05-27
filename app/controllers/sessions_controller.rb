61f1c2445d7456a5f238f13ea36c19054162060d
AReid987 <read.musik@gmail.com>
2018-05-26 20:54:31 -0500

    added sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+  @user = User.find_by(name: params[:user][:name])
+  if @user && @user.authenticate(params[:user][:password])
+    session[:user_id] = @user.id
+    redirect_to user_path(@user), notice: "Sign in success!"
+  else
+    redirect_to signin_path
   end
+end
+
 
   def destroy
     session[:user_id] = nil
