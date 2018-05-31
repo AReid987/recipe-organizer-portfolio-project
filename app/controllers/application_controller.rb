fb107a721b9cddaae6d816c1769a71dda6c4281d
AReid987 <read.musik@gmail.com>
2018-05-31 14:00:22 -0500

    app controller require_logged_in method and before action, logged_in? method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :current_user
+  before_action :require_logged_in, except: [:new, :create, :home, :create_facebook]
+
+  def logged_in?
+   !!current_user
+  end
 
   private
+
+  def require_logged_in
+    redirect_to root_path, notice: "You must be logged in to do that" unless logged_in?
+
+  end
 
   def log_user_in
     session[:user_id] = @user.id
