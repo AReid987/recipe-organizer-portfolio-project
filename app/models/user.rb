44fd4721bad14527cd7a1b9d97c90dd751f1ea1b
AReid987 <read.musik@gmail.com>
2018-05-31 12:11:21 -0500

    add .from_omniauth class method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   validates :username, uniqueness: true
+
+  def self.from_omniauth(auth)
+  where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+    user.provider = auth.provider
+    user.uid = auth.uid
+    user.name = auth.info.name unless user.name != nil
+    user.email =  SecureRandom.hex + '@example.com' unless user.email != nil
+    #user.activated = true
+    user.password = SecureRandom.urlsafe_base64 unless user.password != nil
+    user.save!
+  end
+end
 end
