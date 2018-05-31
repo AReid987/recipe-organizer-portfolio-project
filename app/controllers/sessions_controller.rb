677585752f2e542b7b1369dac3f61c8bd2910d83
AReid987 <read.musik@gmail.com>
2018-05-31 12:05:18 -0500

    #create_facebook method for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,20 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:user][:username])
-      if @user && @user.authenticate(params[:user][:password])
-        session[:user_id] = @user.id
-        redirect_to user_path(@user), notice: "Sign in success!"
-      else
-        flash[:notice] = "Incorrect username or password"
-        redirect_to signin_path
-      end
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user), notice: "Sign in success!"
+    else
+      flash[:notice] = "Incorrect username or password"
+      redirect_to signin_path
     end
+  end
+
+  def create_facebook
+    @user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = @user.id
+    redirect_to user_path(@user), notice: "You are logged in with Facebook!"
+  end
 
 
   def destroy
@@ -23,7 +29,7 @@ class SessionsController < ApplicationController
   end
 
   private
-  
+
   def auth
     request.env['omniauth.auth']
   end
