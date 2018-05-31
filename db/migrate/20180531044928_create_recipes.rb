19d3b6700fcd18c17c77e1d26e2b63cc66bfba63
AReid987 <read.musik@gmail.com>
2018-05-31 00:12:37 -0500

    added columns to recipes table

--- db/migrate/20180531044928_create_recipes.rb
+++ db/migrate/20180531044928_create_recipes.rb
@@ -1,7 +1,9 @@
 class CreateRecipes < ActiveRecord::Migration[5.2]
   def change
     create_table :recipes do |t|
-
+      t.integer :user_id
+      t.string :name 
+      t.string :instructions
       t.timestamps
     end
   end
