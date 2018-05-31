e861808860c3648a536d244c7babe93464220dd8
AReid987 <read.musik@gmail.com>
2018-05-31 11:49:46 -0500

    add private method #auth to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,20 +6,26 @@ class SessionsController < ApplicationController
   end
 
   def create
-  @user = User.find_by(username: params[:user][:username])
-  if @user && @user.authenticate(params[:user][:password])
-    session[:user_id] = @user.id
-    redirect_to user_path(@user), notice: "Sign in success!"
-  else
-    flash[:notice] = "Incorrect username or password"
-    redirect_to signin_path
-  end
-end
+    @user = User.find_by(username: params[:user][:username])
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user), notice: "Sign in success!"
+      else
+        flash[:notice] = "Incorrect username or password"
+        redirect_to signin_path
+      end
+    end
 
 
   def destroy
     session[:user_id] = nil
     redirect_to root_url
+  end
+
+  private
+  
+  def auth
+    request.env['omniauth.auth']
   end
 
 end
