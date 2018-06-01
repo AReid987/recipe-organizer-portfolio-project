d006bc45a01cfa72daf863561822ede00ebc09cd
AReid987 <read.musik@gmail.com>
2018-05-31 19:59:53 -0500

    added accepts_nested_attributes_for ingredients to recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :items
   has_many :ingredients, through: :items
-
+  accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
+  
   validates :name, presence: true
 end
