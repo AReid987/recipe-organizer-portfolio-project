62c140ab9f1679a0c2108368c152a3e6e1eea42d
AReid987 <read.musik@gmail.com>
2018-05-31 19:33:28 -0500

    generated item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_31_165700) do
+ActiveRecord::Schema.define(version: 2018_06_01_002627) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.integer "user_id"
