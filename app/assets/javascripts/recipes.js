26100fe36e9a5595bec62c333a79041940dab51e
AReid987 <read.musik@gmail.com>
2018-07-09 16:13:12 -0500

    removed comments from recipe.js append recipes to DOM

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,13 +1,13 @@
 $(function(){
   $("a.user_recipes").on("click", function(e){
-  //   $.ajax({
-  //     method: 'GET',
-  //     url: this.href,
-  //   }).done(function(response){
-  //     $('#recipes').html(response)
-  //   })
+
   $.get(this.href).success(function(json){
-    debugger
+    let $ol = $('#recipes ol')
+    $ol.text('')
+    json.forEach(function(recipe){
+      $ol.append('<li>' + recipe.name + '</li>')
+    })
+
   })
     e.preventDefault()
   })
