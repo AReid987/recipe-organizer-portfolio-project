c223b38314b9671d8fa98ad7442939f8ebd5c7ee
AReid987 <read.musik@gmail.com>
2018-07-10 17:08:59 -0500

    abstracted get request to user recipes to getUserRecipes function

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,20 +1,6 @@
 $(function(){
   $("a.user_recipes").on("click", function(e){
-
-  $.get(this.href).success(function(json){
-    let $ul = $('#recipes ul')
-    $ul.text('')
-    let $ol = $('#recipes ol')
-    $ol.text('')
-    json.forEach(function(recipe){
-      $ul.html('<h4><li>' + recipe.name + '</li></h4>' +
-      '<h4>Instructions: </h4>' +
-      '<h4><li>' + recipe.instructions + '</li></h4>')
-      recipe.items.forEach(function(item){
-        $ol.append('<li>' + item.ingredient.name + ' - ' + item.quantity + '</li>')
-      })
-    })
-  })
+    getUserRecipes(this)
     e.preventDefault()
   })
 
@@ -68,3 +54,19 @@ $(function(){
   })
 
 })
+function getUserRecipes(link){
+  $.get(link.href).success(function(json){
+    let $ul = $('#recipes ul')
+    $ul.text('')
+    let $ol = $('#recipes ol')
+    $ol.text('')
+    json.forEach(function(recipe){
+      $ul.html('<h4><li>' + recipe.name + '</li></h4>' +
+      '<h4>Instructions: </h4>' +
+      '<h4><li>' + recipe.instructions + '</li></h4>')
+      recipe.items.forEach(function(item){
+        $ol.append('<li>' + item.ingredient.name + ' - ' + item.quantity + '</li>')
+      })
+    })
+  })
+}
