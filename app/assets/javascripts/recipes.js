be91db2055a9f3e13fc4d51feb93b910f3739f3d
AReid987 <read.musik@gmail.com>
2018-07-10 21:56:45 -0500

    abstracted logic into getOneRecipe function

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -36,40 +36,44 @@ function getAllRecipes(link){
     json.forEach(function(recipe){
       $ul.append(`<li><a href="/recipes/${recipe.id}" id="recipe${recipe.id}">${recipe.name}</a></li>`)
       $(`#recipe${recipe.id}`).on('click', function(e){
-        $.get(this.href).success(function(json){
-          let $ol = $('#recipes ol')
-          $ol.text('')
-          let $ul = $('#recipes ul')
-          $ul.text('')
-          $ul.html('<h4><li>' + json.name + '</li></h4>' +
-          '<h4>Instructions: </h4>' +
-          '<h4><li>' + json.instructions + '</li></h4>')
-          json.items.forEach(function(item){
-            $ol.append('<li>' + item.ingredient.name + ' - ' + item.quantity + '</li>')
-          })
-          $('#recipes span').html(`<a href="#" class="js-next" data-id="${json.id}">next</a>`)
-          $('.js-next').on('click', function(e){
-            var nextId = parseInt($(".js-next").attr("data-id")) + 1
-            $.get(`/recipes/${nextId}`).success(function(json){
-              let $ol = $('#recipes ol')
-              $ol.text('')
-              let $ul = $('#recipes ul')
-              $ul.text('')
-              $ul.html('<h4><li>' + json.name + '</li></h4>' +
-              '<h4>Instructions: </h4>' +
-              '<h4><li>' + json.instructions + '</li></h4>')
-              json.items.forEach(function(item){
-                $ol.append('<li>' + item.ingredient.name + ' - ' + item.quantity + '</li>')
-              })
-              $('#recipes span').html(`<a href="#" class="js-next" data-id="${json.id}">next</a>`)
-            })
-            e.preventDefault()
-          })
-        })
-
+        getOneRecipe(this)
         e.preventDefault()
       })
 
     })
   })
+}
+
+function getOneRecipe(link){
+  $.get(link.href).success(function(json){
+    let $ol = $('#recipes ol')
+    $ol.text('')
+    let $ul = $('#recipes ul')
+    $ul.text('')
+    $ul.html('<h4><li>' + json.name + '</li></h4>' +
+    '<h4>Instructions: </h4>' +
+    '<h4><li>' + json.instructions + '</li></h4>')
+    json.items.forEach(function(item){
+      $ol.append('<li>' + item.ingredient.name + ' - ' + item.quantity + '</li>')
+    })
+    $('#recipes span').html(`<a href="#" class="js-next" data-id="${json.id}">next</a>`)
+    $('.js-next').on('click', function(e){
+      var nextId = parseInt($(".js-next").attr("data-id")) + 1
+      $.get(`/recipes/${nextId}`).success(function(json){
+        let $ol = $('#recipes ol')
+        $ol.text('')
+        let $ul = $('#recipes ul')
+        $ul.text('')
+        $ul.html('<h4><li>' + json.name + '</li></h4>' +
+        '<h4>Instructions: </h4>' +
+        '<h4><li>' + json.instructions + '</li></h4>')
+        json.items.forEach(function(item){
+          $ol.append('<li>' + item.ingredient.name + ' - ' + item.quantity + '</li>')
+        })
+        $('#recipes span').html(`<a href="#" class="js-next" data-id="${json.id}">next</a>`)
+      })
+      e.preventDefault()
+    })
+  })
+
 }
