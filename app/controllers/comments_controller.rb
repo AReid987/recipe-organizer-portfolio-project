ed090d2c00679ba759f676d5bcfb8d1d905b9cf3
AReid987 <read.musik@gmail.com>
2018-07-17 20:26:40 -0500

    added index action and #set_recipe to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  before_action :set_recipe
+
+  def index
+    @comments = Recipe.comments
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:recipe_id])
+  end
 end
