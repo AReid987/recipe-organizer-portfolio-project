731005e0a2d343e4ed23734ad49426cf78b5c52e
AReid987 <read.musik@gmail.com>
2018-07-17 20:06:40 -0500

    added content column to comment table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_05_000909) do
+ActiveRecord::Schema.define(version: 2018_07_18_002015) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
